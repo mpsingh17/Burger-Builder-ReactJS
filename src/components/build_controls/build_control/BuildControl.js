4b2666c2b0c5a67343f825ab8a13246a9ae556f9
studio <studio@adminxs-MacBook-Pro.local>
2019-06-30 19:46:59 +1200

    apply missing class for build controls styles

--- src/components/build_controls/build_control/BuildControl.js
+++ src/components/build_controls/build_control/BuildControl.js
@@ -3,7 +3,7 @@ import styles from './BuildControl.module.css';
 
 const BuildControl = (props) => {
     return (
-        <div>
+        <div className={styles.BuildControl}>
             <div className={styles.Label}>{props.label}</div>
             <button className={styles.Less}>Less</button>
             <button className={styles.More}>More</button>
